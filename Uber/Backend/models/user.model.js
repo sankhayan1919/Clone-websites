fe114c79af7dcc9ddf4423eed26dfcfb21d0e2ef
sankhayan1919 <sankhayansadhukhan71@gmail.com>
2025-03-24 00:59:42 +0530

    some folders added

---
+++ Uber/Backend/models/user.model.js
@@ -0,0 +1,46 @@
+const mogoose = require('mogoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const userSchema = new mongoose.Schema({
+    fullname: {
+        firstname: {
+            type: String,
+            required: true,
+            minlength: [3, 'first name must be at least 3 characters long'],
+            maxlength: [20, 'first name must be at most 20 characters long']
+        },
+        lastname: {
+            type: String,
+            minlength: [3, 'last name must be at least 3 characters long'],
+            maxlength: [20, 'last name must be at most 20 characters long']
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: [5, 'email must be at least 5 characters long'],
+        maxlength: [50, 'email must be at most 50 characters long']
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false
+    },
+    socketId: {
+        type: String,
+        required: true
+    }
+})
+userSchema.methods.generateAuthToken = function(){
+    const token = jwt.sign({_id: this._id}, process.env.JWT_SECRET);
+    return token;
+}
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+userSchema.statics.hashPassword = async function (password) {
+    return await bcrypt.hash(password, 10);
+}
+const userModel = mongoose.model('user', userSchema);
+module.exports = userModel;
